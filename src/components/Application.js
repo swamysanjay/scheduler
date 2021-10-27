8427a9468a261741e3597f022c552d8afef859c7
swamysanjay <swamysanjay98@gmail.com>
2021-10-27 19:13:27 -0400

    used useState to store day state in Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -30,10 +30,10 @@ export default function Application(props) {
         <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler"/>
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList
-        days={days}
-        day={day}
-        setDay={day => console.log(day)}/>
+        <DayList 
+        days={days} 
+        day={day} 
+        setDay={setDay} />
         </nav>
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs"/>
       </section>
