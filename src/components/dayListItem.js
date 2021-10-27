206e613ccdd4502b2a45a56c2fdcca45617f7513
swamysanjay <swamysanjay98@gmail.com>
2021-10-26 20:57:08 -0400

    Created dayListItem.js and scss file and added styling and some functionality to the buttons on click

---
+++ src/components/dayListItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "components/dayListItem.scss";
+const classNames = require('classnames');
+
+export default function DayListItem(props) {
+  let dayClass = classNames('day-list_item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': !props.spots
+  })
+  return (
+    <li 
+    className = {dayClass}
+    onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
