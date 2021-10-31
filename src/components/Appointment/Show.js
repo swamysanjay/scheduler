10150de376873798e6f799fa1f0515055438106e
swamysanjay <swamysanjay98@gmail.com>
2021-10-31 07:35:55 -0400

    Create components to display appointment data

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 export default function Show (props) {
-  const interviewerName = props.interviewer.map(interviewerObject => {
-    return interviewerObject.name;
-  })
-
+  const interviewerName = props.interviewer.map(interviewerObject => interviewerObject.interviewer);
+  console.log("props", props);
+  console.log(interviewerName); 
+  
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
