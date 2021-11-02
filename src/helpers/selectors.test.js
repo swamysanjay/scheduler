44f141ddc051902b48f0ce6e9993016fe98b5444
swamysanjay <swamysanjay98@gmail.com>
2021-11-02 15:23:47 -0400

    Added custom hooks and fixed couple bug issues

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -67,7 +67,6 @@ test("getAppointmentsForDay returns an empty array when the day is not found", (
   const result = getAppointmentsForDay(state, "Wednesday");
   expect(result.length).toEqual(0);
 });
-
 test("getInterview returns an object with the interviewer data", () => {
   const result = getInterview(state, state.appointments["3"].interview);
   expect(result).toEqual(
